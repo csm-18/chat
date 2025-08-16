bab325eca84b452877cf49c2793204359b66bd8b
csm-18 <112937168+csm-18@users.noreply.github.com>
2025-08-16 11:01:40 +0530

    add home route to backend

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 
 def main():
-    app = Flask(__name__)
+    app = Flask(__name__, static_folder='./frontend/dist',static_url_path="")
     
 
     @app.route('/')
     def home():
-        return "Hello, World!"
+        return send_from_directory(app.static_folder, 'index.html')
 
     app.run(debug=True)
 
